7a79770bf2b7379bd1a1719a2ffa1d12725c0871
Logon Aluno <logonrmlocal@fiap.com.br>
2023-08-16 20:58:33 -0300

    Adicionei as variaveis capacidade e inicial

--- ApresenteSe/ApresenteSe.kt
+++ ApresenteSe/ApresenteSe.kt
@@ -1,5 +1,9 @@
 fun main () {
     
-    println("Matheus Martins")
+    var nome = "benezinho"
+    var capacidade = 5.1f
+    var inicial = 'S'
+
+    println("A squad se chama $nome, tenho $capacidade anos e minha inicial $inicial")
 }
 
